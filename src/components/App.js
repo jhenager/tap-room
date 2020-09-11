7d31f5ea95a4959ced1b41bb6792abb00504e7ce
James Henager <jameschenager@gmail.com>
2020-09-11 10:31:45 -0700

    add componenets folder and app layout

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./Header";
+import TapControl from "./TapControl";
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <TapControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
