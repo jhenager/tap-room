90c3ac5077d6658af6a412eacc6284161e4f488f
James Henager <jameschenager@gmail.com>
2020-09-11 12:49:41 -0700

    add taplist function and proptypes

---
+++ src/components/TapList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Tap from './Tap';
+import PropTypes from "prop-types";
+
+function TapList(props){
+  return (
+    <React.Fragment>
+      <hr />
+      {props.tapList.map((tap, index) =>
+        <Tap brewery={tap.brewery}
+          beer={tap.beer}
+          style={tap.style}
+          price={tap.price}
+          abv={tap.abv}
+          key={index}/>
+        )}
+    </React.Fragment>
+  );
+}
+
+TapList.proptypes = {
+  tapList: PropTypes.array
+};
+
+export default TapList;
